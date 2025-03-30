8b75194dc55f038570a41fed6f2d9541d88ec901
Ekskie <107729633+Ekskie@users.noreply.github.com>
2025-03-30 16:08:41 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('mamaya-cache').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'firstpage.html',
+                'addingWork.html',
+                'admin.html',
+                'signup.html',
+                'userdashboard.html',
+                'worklist.html',
+                'manifest.json',
+                'malupiton.jpg',
+                'malupiton.jpg'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
